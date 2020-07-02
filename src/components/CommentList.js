b0b64c160f14fdea2901c486ab9aaaacce01f3c8
TylerKonesky <tylerkonesky@hotmail.com>
2020-07-01 19:54:10 -0600

    first tests

---
+++ src/components/CommentList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class CommentList extends Component{
+    render(){
+        return(
+            <div>
+                This is the comment List
+            </div>
+        )
+    }
+}
+
+export default CommentList
